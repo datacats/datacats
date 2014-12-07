ba63586bec2d4124aca23fbbbdc00bfcae7a50df
Denis Zgonjanin <deniszgonjanin@gmail.com>
2014-12-07 16:18:10 -0500

    fix who.ini missing due to volume not mounted properly

--- build.sh
+++ build.sh
@@ -8,32 +8,44 @@ docker build -t datacats_db postgresql/
 docker build -t datacats_web web/
 
 #Setup the virtualenv
-id=$(cat setup_ckan.sh | docker run -i -a stdin -v $DATACATS_FILES/test/venv:/usr/lib/ckan/ datacats_web /bin/bash -c "cat | sh")
+id=$(cat setup_ckan.sh | docker run -i -a stdin \
+    -v $DATACATS_FILES/test/venv:/usr/lib/ckan/ \
+    -v $DATACATS_FILES/test/ini:/etc/ckan/default \
+    datacats_web /bin/bash -c "cat | sh")
 test $(docker wait $id) -eq 0
 
 #Run postgres
 docker run -d --name=datacats_db_test datacats_db
 
 #Run Solr
-docker run -d --name=datacats_solr_test -v $DATACATS_FILES/test/venv/src/ckan/ckan/config/solr/schema.xml:/etc/solr/conf/schema.xml datacats_solr
+docker run -d --name=datacats_solr_test \
+    -v $DATACATS_FILES/test/venv/src/ckan/ckan/config/solr/schema.xml:/etc/solr/conf/schema.xml \
+    datacats_solr
 
 #Initialize the ckan .ini files
-id=$(cat init_ini.sh | docker run -i -a stdin -v $DATACATS_FILES/test/venv:/usr/lib/ckan -v $DATACATS_FILES/test/ini:/etc/ckan/default datacats_web /bin/bash -c "cat | sh")
+id=$(cat init_ini.sh | docker run -i -a stdin \
+    -v $DATACATS_FILES/test/venv:/usr/lib/ckan \
+    -v $DATACATS_FILES/test/ini:/etc/ckan/default \
+    datacats_web /bin/bash -c "cat | sh")
 test $(docker wait $id) -eq 0
 
 #Initialize the database
 id=$(docker run -i -a stdin \
     -v $DATACATS_FILES/test/venv:/usr/lib/ckan \
-    -v $DATACATS_FILES/test/ini/production.ini:/etc/ckan/default/production.ini \
+    -v $DATACATS_FILES/test/ini/:/etc/ckan/default/ \
     --link datacats_solr_test:solr \
     --link datacats_db_test:db \
     datacats_web /usr/lib/ckan/bin/paster --plugin=ckan db init -c /etc/ckan/default/production.ini)
 test $(docker wait $id) -eq 0
 
+#copy the wsgi file into the mounted host volume
+#I don't like the way this is done, but I can't think of a better way
+cp web/apache.wsgi $DATACATS_FILES/test/ini/apache.wsgi
+
 #Run the web container
 docker run --name=datacats_web_test -it \
     -v $DATACATS_FILES/test/venv:/usr/lib/ckan \
-    -v $DATACATS_FILES/test/ini/production.ini:/etc/ckan/default/production.ini \
+    -v $DATACATS_FILES/test/ini/:/etc/ckan/default/ \
     --link datacats_solr_test:solr \
     --link datacats_db_test:db \
     -p 80:80 \
--- setup_ckan.sh
+++ setup_ckan.sh
@@ -6,4 +6,4 @@ chown -R www-data:www-data /var/www/
 git clone --depth 1 https://github.com/ckan/ckan.git $CKAN_HOME/src/ckan
 $CKAN_HOME/bin/pip install -r $CKAN_HOME/src/ckan/requirements.txt
 $CKAN_HOME/bin/pip install -e $CKAN_HOME/src/ckan/
-ln -s $CKAN_HOME/src/ckan/ckan/config/who.ini $CKAN_CONFIG/who.ini
+cp $CKAN_HOME/src/ckan/ckan/config/who.ini $CKAN_CONFIG/who.ini
