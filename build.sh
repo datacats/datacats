1e3d6fede0f0ac5ef513e42d06c25dec05042bcd
Denis Zgonjanin <deniszgonjanin@gmail.com>
2014-12-08 00:37:55 -0500

    allow setting name of project and ckan branch to use to initialize it

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 #Script that gives you a test Datacats environment, to check the workflow works
 
+NAME=${1:-master}
+BRANCH=${2:-master}
+
+rm -rf $DATACATS_FILES/$NAME/
+mkdir -p $DATACATS_FILES/$NAME/
+
 #Build the images
 docker build -t datacats_solr solr/
 docker build -t datacats_db postgresql/
@@ -9,44 +15,45 @@ docker build -t datacats_web web/
 
 #Setup the virtualenv
 id=$(cat setup_ckan.sh | docker run -i -a stdin \
-    -v $DATACATS_FILES/test/venv:/usr/lib/ckan/ \
-    -v $DATACATS_FILES/test/ini:/etc/ckan/default \
+    -e "BRANCH=$BRANCH" \
+    -v $DATACATS_FILES/$NAME/venv:/usr/lib/ckan/ \
+    -v $DATACATS_FILES/$NAME/ini:/etc/ckan/default/ \
     datacats_web /bin/bash -c "cat | sh")
 test $(docker wait $id) -eq 0
 
 #Run postgres
-docker run -d --name=datacats_db_test datacats_db
+docker run -d --name="datacats_db_${NAME}" datacats_db
 
 #Run Solr
-docker run -d --name=datacats_solr_test \
-    -v $DATACATS_FILES/test/venv/src/ckan/ckan/config/solr/schema.xml:/etc/solr/conf/schema.xml \
+docker run -d --name="datacats_solr_${NAME}" \
+    -v $DATACATS_FILES/$NAME/venv/src/ckan/ckan/config/solr/schema.xml:/etc/solr/conf/schema.xml \
     datacats_solr
 
 #Initialize the ckan .ini files
 id=$(cat init_ini.sh | docker run -i -a stdin \
-    -v $DATACATS_FILES/test/venv:/usr/lib/ckan \
-    -v $DATACATS_FILES/test/ini:/etc/ckan/default \
+    -v $DATACATS_FILES/$NAME/venv:/usr/lib/ckan \
+    -v $DATACATS_FILES/$NAME/ini:/etc/ckan/default \
     datacats_web /bin/bash -c "cat | sh")
 test $(docker wait $id) -eq 0
 
 #Initialize the database
 id=$(docker run -i -a stdin \
-    -v $DATACATS_FILES/test/venv:/usr/lib/ckan \
-    -v $DATACATS_FILES/test/ini/:/etc/ckan/default/ \
-    --link datacats_solr_test:solr \
-    --link datacats_db_test:db \
+    -v $DATACATS_FILES/$NAME/venv:/usr/lib/ckan \
+    -v $DATACATS_FILES/$NAME/ini/:/etc/ckan/default/ \
+    --link "datacats_solr_${NAME}":solr \
+    --link "datacats_db_${NAME}":db \
     datacats_web /usr/lib/ckan/bin/paster --plugin=ckan db init -c /etc/ckan/default/production.ini)
 test $(docker wait $id) -eq 0
 
 #copy the wsgi file into the mounted host volume
 #I don't like the way this is done, but I can't think of a better way
-cp web/apache.wsgi $DATACATS_FILES/test/ini/apache.wsgi
+cp web/apache.wsgi $DATACATS_FILES/$NAME/ini/apache.wsgi
 
 #Run the web container
-docker run --name=datacats_web_test -it \
-    -v $DATACATS_FILES/test/venv:/usr/lib/ckan \
-    -v $DATACATS_FILES/test/ini/:/etc/ckan/default/ \
-    --link datacats_solr_test:solr \
-    --link datacats_db_test:db \
-    -p 80:80 \
+docker run --name="datacats_web_${NAME}" -it \
+    -v $DATACATS_FILES/$NAME/venv:/usr/lib/ckan \
+    -v $DATACATS_FILES/$NAME/ini/:/etc/ckan/default/ \
+    --link "datacats_solr_${NAME}":solr \
+    --link "datacats_db_${NAME}":db \
+    -p 80 \
     datacats_web
--- setup_ckan.sh
+++ setup_ckan.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+echo $BRANCH
+
 virtualenv $CKAN_HOME #change to use site packages
 mkdir -p $CKAN_HOME $CKAN_CONFIG /var/www/storage
 chown -R www-data:www-data /var/www/
-git clone --depth 1 https://github.com/ckan/ckan.git $CKAN_HOME/src/ckan
+git clone --depth 1 --branch $BRANCH https://github.com/ckan/ckan.git $CKAN_HOME/src/ckan
 $CKAN_HOME/bin/pip install -r $CKAN_HOME/src/ckan/requirements.txt
 $CKAN_HOME/bin/pip install -e $CKAN_HOME/src/ckan/
 cp $CKAN_HOME/src/ckan/ckan/config/who.ini $CKAN_CONFIG/who.ini
