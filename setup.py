e5e3c0055db0abbfba0233a01753d693288b568c
Ian Ward <ian@excess.org>
2014-12-09 18:41:47 -0500

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+install_requires=[
+    'setuptools',
+    'docopt',
+]
+
+setup(
+    name='datacats',
+    version='0.1',
+    description=
+        'The easiest way to develop and deploy CKAN cross-platform.'
+    license='AGPL3',
+    author='Boxkite',
+    author_email='contact@boxkite.ca',
+    url='https://github.com/dcats/datacats',
+    packages=[
+        'datacats',
+        'datacats.tests',
+        ],
+    install_requires=install_requires,
+    test_suite='datacats.tests',
+    zip_safe=False,
+    entry_points = """
+        [console_scripts]
+        datacats=datacats.cli:main
+        """
+    )
+
