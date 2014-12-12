99e0a0b5b7655fe682037eebf4c22359088ae2ab
Ian Ward <ian@excess.org>
2014-12-12 17:51:16 -0500

    messages for each part of build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -7,8 +7,9 @@ NAME="${1:-master}"
 BRANCH="${2:-master}"
 WORKDIR="$(readlink -f "${3:-.}")"
 DATADIR="${HOME}/.datacats/${NAME}"
-
 TARGET="$WORKDIR/$NAME"
+
+echo Creating CKAN Project
 mkdir -p "$DATADIR"
 mkdir "$DATADIR/venv"
 mkdir "$DATADIR/solr"
@@ -18,7 +19,7 @@ mkdir "$TARGET"
 mkdir "$TARGET/ini"
 mkdir "$TARGET/src"
 
-#Setup the virtualenv  FIXME: based on ckan/master deps for now
+# FIXME: based on ckan/master deps for now
 docker run --rm -i \
     -e "BRANCH=$BRANCH" \
     -v "$DATADIR/venv:/usr/lib/ckan" \
@@ -27,24 +28,22 @@ docker run --rm -i \
     datacats_web:master /bin/bash < init_project.sh
 
 echo Starting DB
-#Run postgres
 docker run -d --name="datacats_db_${NAME}" datacats_db
 
 echo Starting Solr
-#Run Solr
 docker run -d --name="datacats_solr_${NAME}" \
     -v "$DATADIR/solr:/var/lib/solr" \
     -v "$TARGET/src/ckan/ckan/config/solr/schema.xml:/etc/solr/conf/schema.xml" \
     datacats_solr
 
-#Initialize the ckan .ini files
+echo Creating INI files
 docker run --rm -i \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$TARGET/src:/project/src" \
     -v "$TARGET/ini:/etc/ckan/default" \
     datacats_web /bin/bash < init_ini.sh
 
-#Initialize the database
+echo Creating DB
 docker run --rm -i \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$TARGET/src:/project/src" \
@@ -53,7 +52,7 @@ docker run --rm -i \
     --link "datacats_db_${NAME}":db \
     datacats_web /usr/lib/ckan/bin/paster --plugin=ckan db init -c /etc/ckan/default/ckan.ini
 
-#Run the web container
+echo Starting web server
 docker run --name="datacats_web_${NAME}" -it \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$TARGET/src:/project/src" \
