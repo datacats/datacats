10c6064d36d5ec5b21c904cf709dd480f5809509
Ian Ward <ian@excess.org>
2014-12-13 21:53:16 -0500

    polish build output

--- docker/build.sh
+++ docker/build.sh
@@ -10,7 +10,7 @@ WORKDIR="$(readlink -f "${3:-.}")"
 DATADIR="${HOME}/.datacats/${NAME}"
 TARGET="$WORKDIR/$NAME"
 
-echo Creating CKAN Project
+echo '[1/3] Creating Project "'$NAME'"'
 mkdir -p "$DATADIR"
 mkdir "$DATADIR/venv"
 mkdir "$DATADIR/solr"
@@ -30,25 +30,23 @@ docker run --rm -i \
     -v "$TARGET/ini:/etc/ckan/default" \
     datacats_web:master /bin/bash < init_project.sh
 
-echo Starting DB
 docker run --name="datacats_db_${NAME}" \
     -v "$DATADIR/db:/var/lib/postgresql/data" \
     datacats_db > "$TARGET/logs/db.log" 2>&1 &
 
-echo Starting Solr
 docker run --name="datacats_solr_${NAME}" \
     -v "$DATADIR/solr:/var/lib/solr" \
     -v "$TARGET/src/ckan/ckan/config/solr/schema.xml:/etc/solr/conf/schema.xml" \
     datacats_solr > "$TARGET/logs/solr.log" 2>&1 &
 
-echo Creating INI files
+echo '[2/3] Creating INI files'
 docker run --rm -i \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$TARGET/src:/project/src" \
     -v "$TARGET/ini:/etc/ckan/default" \
     datacats_web /bin/bash < init_ini.sh > /dev/null
 
-echo Creating DB
+echo '[3/3] Creating DB'
 docker run --rm -i \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$DATADIR/storage:/var/www/storage" \
@@ -56,19 +54,8 @@ docker run --rm -i \
     -v "$TARGET/ini:/etc/ckan/default" \
     --link "datacats_solr_${NAME}":solr \
     --link "datacats_db_${NAME}":db \
-    datacats_web /usr/lib/ckan/bin/paster --plugin=ckan db init -c /etc/ckan/default/ckan.ini
+    datacats_web /usr/lib/ckan/bin/paster --plugin=ckan db init -c /etc/ckan/default/ckan.ini > /dev/null 2>&1
 
-echo Creating initial sysadmin user
-docker run --rm -it \
-    -v "$DATADIR/venv:/usr/lib/ckan" \
-    -v "$DATADIR/storage:/var/www/storage" \
-    -v "$TARGET/src:/project/src" \
-    -v "$TARGET/ini:/etc/ckan/default" \
-    --link "datacats_solr_${NAME}":solr \
-    --link "datacats_db_${NAME}":db \
-    datacats_web /usr/lib/ckan/bin/paster --plugin=ckan sysadmin add admin -c /etc/ckan/default/ckan.ini
-
-echo Starting web server
 docker run --name="datacats_web_${NAME}" \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$DATADIR/storage:/var/www/storage" \
@@ -81,7 +68,20 @@ docker run --name="datacats_web_${NAME}" \
 
 IP=""
 while true; do
-    IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "datacats_web_${NAME}") 2>/dev/null || true
+    IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "datacats_web_${NAME}" 2>/dev/null) || true
     if [ "$IP" == "" ]; then sleep 0.1; continue; fi
     break
 done
+
+echo "Site available at http://$IP/"
+echo
+echo Creating Initial Sysadmin User
+docker run --rm -it \
+    -v "$DATADIR/venv:/usr/lib/ckan" \
+    -v "$DATADIR/storage:/var/www/storage" \
+    -v "$TARGET/src:/project/src" \
+    -v "$TARGET/ini:/etc/ckan/default" \
+    --link "datacats_solr_${NAME}":solr \
+    --link "datacats_db_${NAME}":db \
+    datacats_web /usr/lib/ckan/bin/paster --plugin=ckan sysadmin add admin -c /etc/ckan/default/ckan.ini
+
