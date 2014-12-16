b92378596d798efa61a8634fd374f0e8b00d36f5
Ian Ward <ian@excess.org>
2014-12-16 12:03:51 -0500

    web,data,search images

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-docker build -t datacats/solr solr/
-docker build -t datacats/postgres postgres/
+docker build -t datacats/search search/
+docker build -t datacats/data data/
 docker build -t datacats/web web/
 
 docker run -i --name datacats_preload_master \
-    -e BRANCH=master datacats_web \
+    -e BRANCH=master datacats/web \
     /bin/bash < setup_ckan.sh
 docker commit datacats_preload_master datacats/web:preload_master
 docker rm datacats_preload_master
