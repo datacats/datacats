171a1403ac2d0fc0e88f2100b576deae22a2e149
Ian Ward <ian@excess.org>
2014-12-19 17:06:50 -0500

    rename /ini to /conf. solr schema.xml in conf dir

--- docker/build.sh
+++ docker/build.sh
@@ -22,7 +22,7 @@ mkdir "$DATADIR/search"
 mkdir "$DATADIR/data"
 mkdir "$DATADIR/files"
 mkdir "$TARGET"
-mkdir "$TARGET/ini"
+mkdir "$TARGET/conf"
 mkdir "$TARGET/src"
 
 # FIXME: based on ckan/master deps for now
@@ -31,7 +31,7 @@ docker run --rm -i \
     -v "$DATADIR/venv:/usr/lib/ckan_target" \
     -v "$DATADIR/files:/var/www/storage" \
     -v "$TARGET/src:/project/src_target" \
-    -v "$TARGET/ini:/etc/ckan/default" \
+    -v "$TARGET/conf:/etc/ckan/default" \
     datacats/web:preload_master /bin/bash < init_project.sh
 
 docker run -d --name="datacats_data_${NAME}" \
@@ -44,7 +44,7 @@ docker run -d --name="datacats_data_${NAME}" \
 
 docker run -d --name="datacats_search_${NAME}" \
     -v "$DATADIR/search:/var/lib/solr" \
-    -v "$TARGET/src/ckan/ckan/config/solr/schema.xml:/etc/solr/conf/schema.xml" \
+    -v "$TARGET/conf/schema.xml:/etc/solr/conf/schema.xml" \
     datacats/search > /dev/null
 
 echo '[2/3] Creating INI files'
@@ -54,7 +54,7 @@ docker run --rm -i \
     -e DATASTORE_RW_PASSWORD="$DATASTORE_RW_PASSWORD" \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$TARGET/src:/project/src" \
-    -v "$TARGET/ini:/etc/ckan/default" \
+    -v "$TARGET/conf:/etc/ckan/default" \
     datacats/web /bin/bash < init_ini.sh > /dev/null
 
 echo '[3/3] Initializing Database'
@@ -62,7 +62,7 @@ docker run --rm -i \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$DATADIR/files:/var/www/storage" \
     -v "$TARGET/src:/project/src" \
-    -v "$TARGET/ini:/etc/ckan/default" \
+    -v "$TARGET/conf:/etc/ckan/default" \
     --link "datacats_search_${NAME}":solr \
     --link "datacats_data_${NAME}":db \
     datacats/web /usr/lib/ckan/bin/paster --plugin=ckan db init -c /etc/ckan/default/ckan.ini > /dev/null 2>&1
@@ -71,7 +71,7 @@ docker run -d --name="datacats_web_${NAME}" \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$DATADIR/files:/var/www/storage" \
     -v "$TARGET/src:/project/src" \
-    -v "$TARGET/ini:/etc/ckan/default" \
+    -v "$TARGET/conf:/etc/ckan/default" \
     --link "datacats_search_${NAME}":solr \
     --link "datacats_data_${NAME}":db \
     -p 80 \
@@ -91,7 +91,7 @@ docker run --rm -it \
     -v "$DATADIR/venv:/usr/lib/ckan" \
     -v "$DATADIR/files:/var/www/storage" \
     -v "$TARGET/src:/project/src" \
-    -v "$TARGET/ini:/etc/ckan/default" \
+    -v "$TARGET/conf:/etc/ckan/default" \
     --link "datacats_search_${NAME}":solr \
     --link "datacats_data_${NAME}":db \
     datacats/web /usr/lib/ckan/bin/paster --plugin=ckan sysadmin add admin -c /etc/ckan/default/ckan.ini
--- docker/init_project.sh
+++ docker/init_project.sh
@@ -8,7 +8,8 @@ cp -a $CKAN_HOME/. /usr/lib/ckan_target/.
 cp -a $CKAN_SRC/. /project/src_target/.
 chown -R --reference=$CKAN_CONFIG /project/src_target/.
 
-cp $CKAN_SRC/ckan/ckan/config/who.ini $CKAN_CONFIG/who.ini
+cp $CKAN_SRC/ckan/ckan/config/who.ini $CKAN_CONFIG/.
+cp $CKAN_SRC/ckan/ckan/config/solr/schema.xml $CKAN_CONFIG/.
 chown -R --reference=$CKAN_CONFIG /usr/lib/ckan_target/.
 
 chown -R www-data: /var/www
