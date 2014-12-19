97af6f9f01af37dfdba94798804162faa4fd706b
Ian Ward <ian@excess.org>
2014-12-19 17:30:30 -0500

    simplify init_project

--- docker/init_project.sh
+++ docker/init_project.sh
@@ -6,13 +6,10 @@ CKAN_SRC=/project/src
 
 cp -a $CKAN_HOME/. /usr/lib/ckan_target/.
 cp -a $CKAN_SRC/. /project/src_target/.
-chown -R --reference=$CKAN_CONFIG /project/src_target/.
 
-cp $CKAN_SRC/ckan/ckan/config/who.ini $CKAN_CONFIG/.
-cp $CKAN_SRC/ckan/ckan/config/solr/schema.xml $CKAN_CONFIG/.
-chown -R --reference=$CKAN_CONFIG /usr/lib/ckan_target/.
-
-chown -R www-data: /var/www
+cp $CKAN_SRC/ckan/ckan/config/who.ini \
+    $CKAN_SRC/ckan/ckan/config/solr/schema.xml $CKAN_CONFIG/.
+chown -R --reference=$CKAN_CONFIG /usr/lib/ckan_target/. /project/src_target/.
 
 $CKAN_HOME/bin/paster make-config ckan $CKAN_CONFIG/ckan.ini > /dev/null
 $CKAN_HOME/bin/paster --plugin=ckan config-tool "$CKAN_CONFIG/ckan.ini" -e \
