1b9e5189e45990cef5519f16623350b27909c341
Ian Ward <ian@excess.org>
2014-12-19 20:48:01 -0500

    init_project.sh in python

--- datacats/cli/create.py
+++ datacats/cli/create.py
@@ -2,8 +2,10 @@ from string import uppercase, lowercase, digits
 from random import SystemRandom
 from os.path import abspath, split as path_split, expanduser, isdir
 from os import makedirs
+import sys
+import shutil
 
-from datacats.docker import docker
+from datacats.docker import web_command
 from datacats.validate import valid_name
 
 def generate_db_password():
@@ -13,6 +15,10 @@ def generate_db_password():
     """
     chars = uppercase + lowercase + digits
     return ''.join(SystemRandom().choice(chars) for x in xrange(16))
+
+def write(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
 
 def main(opts):
     workdir, name = path_split(abspath(opts['PROJECT']))
@@ -33,25 +39,83 @@ def main(opts):
     datastore_ro_password = generate_db_password()
     datastore_rw_password = generate_db_password()
 
-    print '[1/3] Creating project "{0}"'.format(name)
-    makedirs(datadir + '/venv', mode=0700)
-    makedirs(datadir + '/search', mode=0700)
-    makedirs(datadir + '/data', mode=0700)
-    makedirs(datadir + '/files', mode=0700)
-    makedirs(target + '/ini')
+    write('[1/1] Creating project "{0}"'.format(name))
+    makedirs(datadir, mode=0700)
+    makedirs(datadir + '/venv')
+    makedirs(datadir + '/search')
+    makedirs(datadir + '/data')
+    makedirs(datadir + '/files')
+    makedirs(target + '/conf')
     makedirs(target + '/src')
+    write('.')
 
-    vols = {
-        datadir + '/venv': {'bind': '/usr/lib/ckan_target', 'ro': False},
-        target + '/src': {'bind': '/project/src_target', 'ro': False},
-        target + '/ini': {'bind': '/etc/ckan/default', 'ro': False},
-    }
-    c = docker.create_container(
-        image='datacats/web:preload_master', # FIXME: only master for now
-        environment={'BRANCH': 'master'}, # FIXME
-        command='init_project.sh',
-        volumes=[v['bind'] for v in vols.itervalues()],
-        detach=False)
-    docker.start(
-        container=c['Id'],
-        binds=vols)
+    ckan_version='master'
+
+    # copy virtualenv
+    web_command(
+        command='/bin/cp -a /usr/lib/ckan/. /usr/lib/ckan_target/.',
+        rw={datadir + '/venv': '/usr/lib/ckan_target'},
+        preload_ckan_version=ckan_version)
+    write('.')
+
+    # copy ckan source
+    web_command(
+        command='/bin/cp -a /project/src/. /project/src_target/.',
+        rw={target + '/src': '/project/src_target'},
+        preload_ckan_version=ckan_version)
+    shutil.copy(
+        target + '/src/ckan/ckan/config/who.ini',
+        target + '/conf')
+    shutil.copy(
+        target + '/src/ckan/ckan/config/solr/schema.xml',
+        target + '/conf')
+    write('.')
+
+    # set ownership of file storage dir to container apache user id
+    web_command(
+        command='/bin/chown -R www-data: /var/www/storage',
+        rw={datadir + '/files': '/var/www/storage'})
+    write('.')
+
+    # create initial ckan.ini
+    web_command(
+        command='/usr/lib/ckan/bin/paster make-config'
+            ' ckan /etc/ckan/default/ckan.ini',
+        ro={datadir + '/venv': '/usr/lib/ckan',
+            target + '/src': '/project/src'},
+        rw={target + '/conf': '/etc/ckan/default'})
+    write('.')
+
+    # update ckan.ini with our host names and passwords
+    # FIXME: using config-tool inside the container, would be better to
+    # do this locally on the config file
+    web_command(
+        command=[
+            '/usr/lib/ckan/bin/paster',
+            '--plugin=ckan',
+            'config-tool',
+            '/etc/ckan/default/ckan.ini',
+            '-e',
+            'sqlalchemy.url = postgresql://ckan:{0}@db:5432/ckan'.format(
+                postgres_password),
+            'ckan.datastore.read_url = postgresql://ckan_datastore_readonly:'
+                '{0}@db:5432/ckan_datastore'.format(datastore_ro_password),
+            'ckan.datastore.write_url = postgresql://ckan_datastore_readwrite:'
+                '{0}@db:5432/ckan_datastore'.format(datastore_rw_password),
+            'solr_url = http://solr:8080/solr',
+            'ckan.storage_path = /var/www/storage',
+            ],
+        ro={datadir + '/venv': '/usr/lib/ckan',
+            target + '/src': '/project/src'},
+        rw={target + '/conf': '/etc/ckan/default'})
+    write('.')
+
+    # set ownership of project files to local user id
+    web_command(
+        command='/bin/chown -R --reference=/etc/ckan/default'
+            ' /usr/lib/ckan /project/src /etc/ckan/default',
+        rw={datadir + '/venv': '/usr/lib/ckan',
+            target + '/src': '/project/src',
+            target + '/conf': '/etc/ckan/default'})
+    write('.')
+
--- datacats/docker.py
+++ datacats/docker.py
@@ -5,4 +5,52 @@ from os import environ
 from docker import Client
 
 docker_host = environ.get('DOCKER_HOST', 'unix://var/run/docker.sock')
-docker = Client(base_url=docker_host)
+_docker = Client(base_url=docker_host)
+
+
+def volumes_from_ro_rw(ro, rw):
+    """
+    ro and rw {localdir: binddir} dicts to docker-py's
+    {localdir: {'bind': binddir, 'ro': T/F}} dicts
+    """
+    out = {}
+    if ro:
+        for localdir, binddir in ro.iteritems():
+            out[localdir] = {'bind': binddir, 'ro': True}
+    if rw:
+        for localdir, binddir in rw.iteritems():
+            out[localdir] = {'bind': binddir, 'ro': False}
+    return out
+
+def binds_from_volumes(volumes):
+    """
+    Return the target 'bind' dirs of volumnes from a volumes dict
+    for passing to create_container
+    """
+    return [v['bind'] for v in volumes.itervalues()]
+
+def web_command(command, ro=None, rw=None, preload_ckan_version=None):
+    """
+    Run a single command in a web image preloaded with the ckan
+    source and virtual envrionment.
+
+    :param command: command to execute
+    :param ro: {localdir: binddir} dict for read-only volumes
+    :param rw: {localdir: binddir} dict for read-write volumes
+    :param preload_ckan_version:
+    """
+    volumes = volumes_from_ro_rw(ro, rw)
+
+    image = 'datacats/web'
+    if preload_ckan_version:
+        image='datacats/web:preload_{0}'.format(preload_ckan_version),
+
+    c = _docker.create_container(
+        image=image,
+        command=command,
+        volumes=[v['bind'] for v in volumes.itervalues()],
+        detach=False)
+    _docker.start(
+        container=c['Id'],
+        binds=binds_from_volumes(volumes))
+    return _docker.wait(c['Id'])
