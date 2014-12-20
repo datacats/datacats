dae3d5246310d4d952d8e8578d4b786605a83e00
Ian Ward <ian@excess.org>
2014-12-19 21:09:21 -0500

    fix bugs

--- datacats/docker.py
+++ datacats/docker.py
@@ -7,11 +7,14 @@ from docker import Client
 docker_host = environ.get('DOCKER_HOST', 'unix://var/run/docker.sock')
 _docker = Client(base_url=docker_host)
 
+class WebCommandError(Exception):
+    pass
 
-def volumes_from_ro_rw(ro, rw):
+
+def ro_rw_to_binds(ro, rw):
     """
     ro and rw {localdir: binddir} dicts to docker-py's
-    {localdir: {'bind': binddir, 'ro': T/F}} dicts
+    {localdir: {'bind': binddir, 'ro': T/F}} binds dicts
     """
     out = {}
     if ro:
@@ -22,7 +25,7 @@ def volumes_from_ro_rw(ro, rw):
             out[localdir] = {'bind': binddir, 'ro': False}
     return out
 
-def binds_from_volumes(volumes):
+def binds_to_volumes(volumes):
     """
     Return the target 'bind' dirs of volumnes from a volumes dict
     for passing to create_container
@@ -39,18 +42,20 @@ def web_command(command, ro=None, rw=None, preload_ckan_version=None):
     :param rw: {localdir: binddir} dict for read-write volumes
     :param preload_ckan_version:
     """
-    volumes = volumes_from_ro_rw(ro, rw)
+    binds = ro_rw_to_binds(ro, rw)
 
     image = 'datacats/web'
     if preload_ckan_version:
-        image='datacats/web:preload_{0}'.format(preload_ckan_version),
+        image='datacats/web:preload_{0}'.format(preload_ckan_version)
 
     c = _docker.create_container(
         image=image,
         command=command,
-        volumes=[v['bind'] for v in volumes.itervalues()],
+        volumes=binds_to_volumes(binds),
         detach=False)
     _docker.start(
         container=c['Id'],
-        binds=binds_from_volumes(volumes))
-    return _docker.wait(c['Id'])
+        binds=binds)
+    if _docker.wait(c['Id']):
+        raise WebCommandError(command)
+    _docker.remove_container(container=c['Id'])
