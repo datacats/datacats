ef2c5e921a5da69703d5e37cfec70997589bef41
Ian Ward <ian@excess.org>
2014-12-21 14:45:35 -0500

    move create code Project class

--- datacats/cli/create.py
+++ datacats/cli/create.py
@@ -1,163 +1,45 @@
-from os.path import abspath, split as path_split, expanduser, isdir
-from os import makedirs
 import sys
-import shutil
-import subprocess
 
-from datacats.docker import (web_command, run_container, remove_container,
-    inspect_container)
-from datacats.validate import valid_name
-from datacats.project import Project
+from datacats.project import Project, ProjectError
 
 def write(s):
     sys.stdout.write(s)
     sys.stdout.flush()
 
 def main(opts):
-    workdir, name = path_split(abspath(opts['PROJECT']))
-
-    if not valid_name(name):
-        print 'Please choose a project name starting with a letter and'
-        print 'includig only lowercase letters and digits'
+    try:
+        project = Project.new(opts['PROJECT'], 'master')
+    except ProjectError as e:
+        print e.message.format(*e.format_args)
         return
 
-    if not isdir(workdir):
-        print 'Parent directory for project does not exist!'
-        return
+    write('Creating project "{0}"'.format(project.name))
 
-    datadir = expanduser('~/.datacats/' + name)
-    target = workdir + '/' + name
-
-    if isdir(datadir):
-        print 'Project data directory {0} already exists.'.format(datadir)
-        return
-    if isdir(target):
-        print 'Project directory already exists.'
-        return
-
-    project = Project()
-    project.generate_passwords()
-
-    write('Creating project "{0}"'.format(name))
-
-    makedirs(datadir, mode=0700)
-    makedirs(datadir + '/venv')
-    makedirs(datadir + '/search')
-    makedirs(datadir + '/data')
-    makedirs(datadir + '/files')
-    makedirs(target + '/conf')
-    makedirs(target + '/src')
-    write('.')
-
-    ckan_version='master'
-
-    # copy virtualenv
-    web_command(
-        command='/bin/cp -a /usr/lib/ckan/. /usr/lib/ckan_target/.',
-        rw={datadir + '/venv': '/usr/lib/ckan_target'},
-        preload_ckan_version=ckan_version)
-    write('.')
-
-    # copy ckan source
-    web_command(
-        command='/bin/cp -a /project/src/. /project/src_target/.',
-        rw={target + '/src': '/project/src_target'},
-        preload_ckan_version=ckan_version)
-    shutil.copy(
-        target + '/src/ckan/ckan/config/who.ini',
-        target + '/conf')
-    shutil.copy(
-        target + '/src/ckan/ckan/config/solr/schema.xml',
-        target + '/conf')
-    write('.')
-
-    # postgres container needs all its user passwords on first run
-    run_container(
-        name='datacats_data_' + name,
-        image='datacats/data',
-        environment=project.passwords,
-        rw={datadir + '/data': '/var/lib/postgresql/data'})
-    run_container(
-        name='datacats_search_' + name,
-        image='datacats/search',
-        rw={datadir + '/search': '/var/lib/solr'},
-        ro={target + '/conf/schema.xml': '/etc/solr/conf/schema.xml'})
-    write('.')
-
-    # set ownership of file storage dir to container apache user id
-    web_command(
-        command='/bin/chown -R www-data: /var/www/storage',
-        rw={datadir + '/files': '/var/www/storage'})
-    write('.')
-
-    # create initial ckan.ini
-    web_command(
-        command='/usr/lib/ckan/bin/paster make-config'
-            ' ckan /etc/ckan/default/ckan.ini',
-        ro={datadir + '/venv': '/usr/lib/ckan',
-            target + '/src': '/project/src'},
-        rw={target + '/conf': '/etc/ckan/default'})
-    write('.')
-
-    # update ckan.ini with our host names and passwords
-    web_command(
-        command=project.ckan_config_tool_command(),
-        ro={datadir + '/venv': '/usr/lib/ckan',
-            target + '/src': '/project/src'},
-        rw={target + '/conf': '/etc/ckan/default'})
-    write('.')
-
-    # set ownership of project files to local user id
-    web_command(
-        command='/bin/chown -R --reference=/etc/ckan/default'
-            ' /usr/lib/ckan /project/src /etc/ckan/default',
-        rw={datadir + '/venv': '/usr/lib/ckan',
-            target + '/src': '/project/src',
-            target + '/conf': '/etc/ckan/default'})
-    write('.')
-
-    # ckan db init
-    web_command(
-        command='/usr/lib/ckan/bin/paster --plugin=ckan db init'
-            ' -c /etc/ckan/default/ckan.ini',
-        ro={datadir + '/venv': '/usr/lib/ckan',
-            target + '/src': '/project/src',
-            target + '/conf': '/etc/ckan/default'},
-        links={'datacats_search_' + name: 'solr',
-            'datacats_data_' + name: 'db'})
-    write('.')
+    for fn in (
+            project.create_directories,
+            project.create_virtualenv,
+            project.create_source,
+            project.start_data_and_search,
+            project.fix_storage_permissions,
+            project.create_ckan_ini,
+            project.update_ckan_ini,
+            project.fix_project_permissions,
+            project.ckan_db_init,
+            ):
+        fn()
+        write('.')
 
     if opts['--image-only']:
-        remove_container('datacats_data_' + name)
-        remove_container('datacats_search_' + name)
+        project.stop_data_and_search()
         write('\n')
     else:
-        run_container(
-            name='datacats_web_' + name,
-            image='datacats/web',
-            rw={datadir + '/files': '/var/www/storage'},
-            ro={datadir + '/venv': '/usr/lib/ckan',
-                target + '/src': '/project/src',
-                target + '/conf': '/etc/ckan/default'},
-            links={'datacats_search_' + name: 'solr',
-                'datacats_data_' + name: 'db'})
+        project.start_web()
         write('.\n')
-        info = inspect_container('datacats_web_' + name)
-        ip = info['NetworkSettings']['IPAddress']
+        ip = project.web_ipaddress()
         write('Site available at http://{0}/\n'.format(ip))
 
     if opts['--no-sysadmin']:
         return
 
     write('\n')
-    # FIXME: consider switching this to dockerpty
-    # using subprocess for docker client's interactive session
-    subprocess.call([
-        '/usr/bin/docker', 'run', '--rm', '-it',
-        '-v', datadir + '/venv:/usr/lib/ckan:ro',
-        '-v', target + '/src:/project/src:ro',
-        '-v', target + '/conf:/etc/ckan/default:ro',
-        '--link', 'datacats_search_' + name + ':solr',
-        '--link', 'datacats_data_' + name + ':db',
-        'datacats/web', '/usr/lib/ckan/bin/paster', '--plugin=ckan',
-        'sysadmin', 'add', 'admin', '-c' '/etc/ckan/default/ckan.ini'])
+    project.interactive_set_admin_password()
--- datacats/project.py
+++ datacats/project.py
@@ -1,29 +1,151 @@
+from os.path import abspath, split as path_split, expanduser, isdir
+from os import makedirs
+import subprocess
+import shutil
 from string import uppercase, lowercase, digits
 from random import SystemRandom
+
+from datacats.validate import valid_name
+from datacats.docker import (web_command, run_container, remove_container,
+    inspect_container)
+
+class ProjectError(Exception):
+    def __init__(self, message, format_args=()):
+        self.message = message
+        self.format_args = format_args
+        super(ProjectError, self).__init__(message, format_args)
+
 
 class Project(object):
     """
     DataCats project settings object
+
+    Create with Project.new(path) or Project.open(path)
     """
+    def __init__(self, name, target, datadir, ckan_version):
+        self.name = name
+        self.target = target
+        self.datadir = datadir
+        self.ckan_version = ckan_version
 
-    def generate_passwords(self):
+    @classmethod
+    def new(cls, path, ckan_version):
         """
-        Generate new DB passwords and store them in self.passwords
-        """
-        self.passwords = {
-            'POSTGRES_PASSWORD': generate_db_password(),
-            'CKAN_PASSWORD': generate_db_password(),
-            'DATASTORE_RO_PASSWORD': generate_db_password(),
-            'DATASTORE_RW_PASSWORD': generate_db_password(),
-            }
+        Return a Project object with settings for a new project.
+        No directories or containers are created by this call.
 
-    def ckan_config_tool_command(self):
+        :params path: location for new project directory, may be relative
+
+        Raises ProjectError if directories or project with same
+        name already exits.
         """
-        Return a config-tool command with all arguments to update a
-        ckan ini file for this project.
+        workdir, name = path_split(abspath(path))
+
+        if not valid_name(name):
+            raise ProjectError('Please choose a project name starting with a '
+                'letter and including only lowercase letters and digits.')
+        if not isdir(workdir):
+            raise ProjectError('Parent directory for project does not exist.')
+
+        datadir = expanduser('~/.datacats/' + name)
+        target = workdir + '/' + name
+
+        if isdir(datadir):
+            raise ProjectError('Project data directory {0} already exists.',
+                (datadir,))
+        if isdir(target):
+            raise ProjectError('Project directory already exists.')
+
+        project = cls(name, target, datadir, ckan_version)
+        project._generate_passwords()
+        return project
+
+    def create_directories(self):
+        """
+        Call once for new projects to create the initial project directories.
+        """
+        makedirs(self.datadir, mode=0o700)
+        makedirs(self.datadir + '/venv')
+        makedirs(self.datadir + '/search')
+        makedirs(self.datadir + '/data')
+        makedirs(self.datadir + '/files')
+        makedirs(self.target + '/conf')
+        makedirs(self.target + '/src')
+
+    def create_virtualenv(self):
+        """
+        Populate venv directory from preloaded image
+        """
+        web_command(
+            command='/bin/cp -a /usr/lib/ckan/. /usr/lib/ckan_target/.',
+            rw={self.datadir + '/venv': '/usr/lib/ckan_target'},
+            preload_ckan_version=self.ckan_version)
+
+    def create_source(self):
+        """
+        Populate src/ckan directory from preloaded image and copy
+        who.ini and schema.xml info conf directory
+        """
+        web_command(
+            command='/bin/cp -a /project/src/. /project/src_target/.',
+            rw={self.target + '/src': '/project/src_target'},
+            preload_ckan_version=self.ckan_version)
+        shutil.copy(
+            self.target + '/src/ckan/ckan/config/who.ini',
+            self.target + '/conf')
+        shutil.copy(
+            self.target + '/src/ckan/ckan/config/solr/schema.xml',
+            self.target + '/conf')
+
+    def start_data_and_search(self):
+        """
+        run the postgres and solr containers
+        """
+        # users are created when data dir is blank so we must pass
+        # all the user passwords as environment vars
+        run_container(
+            name='datacats_data_' + self.name,
+            image='datacats/data',
+            environment=self.passwords,
+            rw={self.datadir + '/data': '/var/lib/postgresql/data'})
+        run_container(
+            name='datacats_search_' + self.name,
+            image='datacats/search',
+            rw={self.datadir + '/search': '/var/lib/solr'},
+            ro={self.target + '/conf/schema.xml': '/etc/solr/conf/schema.xml'})
+
+    def stop_data_and_search(self):
+        """
+        stop and remove postgres and solr containers
+        """
+        remove_container('datacats_data_' + self.name)
+        remove_container('datacats_search_' + self.name)
+
+    def fix_storage_permissions(self):
+        """
+        Set the owner of all apache storage files to www-data container user
+        """
+        web_command(
+            command='/bin/chown -R www-data: /var/www/storage',
+            rw={self.datadir + '/files': '/var/www/storage'})
+
+    def create_ckan_ini(self):
+        """
+        Use make-config to generate an initial ckan.ini file
+        """
+        web_command(
+            command='/usr/lib/ckan/bin/paster make-config'
+                ' ckan /etc/ckan/default/ckan.ini',
+            ro={self.datadir + '/venv': '/usr/lib/ckan',
+                self.target + '/src': '/project/src'},
+            rw={self.target + '/conf': '/etc/ckan/default'})
+
+    def update_ckan_ini(self):
+        """
+        Use config-tool to update ckan.ini with our project settings
         """
         p = self.passwords
-        return [
+        command = [
             '/usr/lib/ckan/bin/paster', '--plugin=ckan', 'config-tool',
             '/etc/ckan/default/ckan.ini', '-e',
             'sqlalchemy.url = postgresql://ckan:'
@@ -35,6 +157,84 @@ class Project(object):
             'solr_url = http://solr:8080/solr',
             'ckan.storage_path = /var/www/storage',
             ]
+        web_command(
+            command=command,
+            ro={self.datadir + '/venv': '/usr/lib/ckan',
+                self.target + '/src': '/project/src'},
+            rw={self.target + '/conf': '/etc/ckan/default'})
+
+    def fix_project_permissions(self):
+        """
+        Reset owner of project files to the host user so they can edit,
+        move and delete them freely.
+        """
+        web_command(
+            command='/bin/chown -R --reference=/etc/ckan/default'
+                ' /usr/lib/ckan /project/src /etc/ckan/default',
+            rw={self.datadir + '/venv': '/usr/lib/ckan',
+                self.target + '/src': '/project/src',
+                self.target + '/conf': '/etc/ckan/default'})
+
+    def ckan_db_init(self):
+        """
+        Run db init to create all ckan tables
+        """
+        web_command(
+            command='/usr/lib/ckan/bin/paster --plugin=ckan db init'
+                ' -c /etc/ckan/default/ckan.ini',
+            ro={self.datadir + '/venv': '/usr/lib/ckan',
+                self.target + '/src': '/project/src',
+                self.target + '/conf': '/etc/ckan/default'},
+            links={'datacats_search_' + self.name: 'solr',
+                'datacats_data_' + self.name: 'db'})
+
+    def _generate_passwords(self):
+        """
+        Generate new DB passwords and store them in self.passwords
+        """
+        self.passwords = {
+            'POSTGRES_PASSWORD': generate_db_password(),
+            'CKAN_PASSWORD': generate_db_password(),
+            'DATASTORE_RO_PASSWORD': generate_db_password(),
+            'DATASTORE_RW_PASSWORD': generate_db_password(),
+            }
+
+    def start_web(self):
+        """
+        Start the apache server or paster serve
+        """
+        run_container(
+            name='datacats_web_' + self.name,
+            image='datacats/web',
+            rw={self.datadir + '/files': '/var/www/storage'},
+            ro={self.datadir + '/venv': '/usr/lib/ckan',
+                self.target + '/src': '/project/src',
+                self.target + '/conf': '/etc/ckan/default'},
+            links={'datacats_search_' + self.name: 'solr',
+                'datacats_data_' + self.name: 'db'})
+
+    def web_ipaddress(self):
+        """
+        Return the IP address of the web server
+        """
+        info = inspect_container('datacats_web_' + self.name)
+        return info['NetworkSettings']['IPAddress']
+
+    def interactive_set_admin_password(self):
+        """
+        launch docker client interactively to set the admin password
+        """
+        # FIXME: consider switching this to dockerpty
+        # using subprocess for docker client's interactive session
+        subprocess.call([
+            '/usr/bin/docker', 'run', '--rm', '-it',
+            '-v', self.datadir + '/venv:/usr/lib/ckan:ro',
+            '-v', self.target + '/src:/project/src:ro',
+            '-v', self.target + '/conf:/etc/ckan/default:ro',
+            '--link', 'datacats_search_' + self.name + ':solr',
+            '--link', 'datacats_data_' + self.name + ':db',
+            'datacats/web', '/usr/lib/ckan/bin/paster', '--plugin=ckan',
+            'sysadmin', 'add', 'admin', '-c' '/etc/ckan/default/ckan.ini'])
 
 
 def generate_db_password():
