1bc47152583d45b43c3ac75435b98cff040f1016
Ian Ward <ian@excess.org>
2015-01-13 13:59:39 -0500

    install: fix reload call

--- datacats/cli/install.py
+++ datacats/cli/install.py
@@ -56,4 +56,7 @@ PROJECT may be a project name or a path to a project directory. Default: '.'
 
     if 'web' in project.containers_running():
         # FIXME: reload without changing debug setting?
-        manage.reload_(project, {'--production': False, 'PORT': None})
+        manage.reload_(project, {
+            '--production': False,
+            'PORT': None,
+            '--background': False})
