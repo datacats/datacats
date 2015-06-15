3452c440d1fb0177697dc89f99aecc3482c15f71
Ian Ward <ian@excess.org>
2015-06-15 17:31:57 -0400

    new install_postgis script

--- datacats/scripts/install_postgis.sh
+++ datacats/scripts/install_postgis.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-su postgres -c "psql -d ckan -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql"
-su postgres -c "psql -d ckan -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql"
-su postgres -c "psql -d ckan -c 'ALTER VIEW geometry_columns OWNER TO ckan;'"
-su postgres -c "psql -d ckan -c 'ALTER TABLE spatial_ref_sys OWNER TO ckan;'"
+set -e
+
+export PGHOST=db
+export PGDATABASE=ckan
+export PGUSER=postgres
+export PGPASSWORD=${DB_ENV_POSTGRES_PASSWORD}
+
+psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql
+psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql
+psql -d ckan -c 'ALTER VIEW geometry_columns OWNER TO ckan;'
+psql -d ckan -c 'ALTER TABLE spatial_ref_sys OWNER TO ckan;'
