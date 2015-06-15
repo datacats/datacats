21aafed2db2e85df43a74ccac5bba8c733b337fa
Ian Ward <ian@excess.org>
2015-06-15 19:07:04 -0400

    install_postgis.sh: skip creating postgis tables if they already exist

--- datacats/scripts/install_postgis.sh
+++ datacats/scripts/install_postgis.sh
@@ -7,7 +7,9 @@ export PGDATABASE=ckan
 export PGUSER=postgres
 export PGPASSWORD=${DB_ENV_POSTGRES_PASSWORD}
 
+psql -c 'SELECT * FROM geometry_columns LIMIT 1;' 2> /dev/null && exit 0
+
 psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql
 psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql
-psql -d ckan -c 'ALTER VIEW geometry_columns OWNER TO ckan;'
-psql -d ckan -c 'ALTER TABLE spatial_ref_sys OWNER TO ckan;'
+psql -c 'ALTER VIEW geometry_columns OWNER TO ckan;'
+psql -c 'ALTER TABLE spatial_ref_sys OWNER TO ckan;'
