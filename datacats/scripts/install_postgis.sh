cc95a6b5e77408b3c43417890c007c4d740983c7
Ian Ward <ian@excess.org>
2015-06-16 18:45:47 -0400

    wait for postgres in install_postgis script

--- datacats/scripts/install_postgis.sh
+++ datacats/scripts/install_postgis.sh
@@ -7,6 +7,13 @@ export PGDATABASE=ckan
 export PGUSER=postgres
 export PGPASSWORD=${DB_ENV_POSTGRES_PASSWORD}
 
+# wait for postgres db to be available, immediately after creation
+# its entrypoint creates the cluster and dbs and this can take a moment
+for tries in 1 2 3 4 5; do
+	psql -c 'SELECT 1;' 2> /dev/null && break
+	sleep 0.3
+done
+
 psql -c 'SELECT * FROM geometry_columns LIMIT 1;' 2> /dev/null && exit 0
 
 psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql
