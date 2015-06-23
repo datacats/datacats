886cef083ac4a4866fee2742ec84f8b4c70feaaf
Jack McCracken <jackamc19@gmail.com>
2015-06-23 15:23:49 -0400

    remove some extra abspaths

--- datacats/scripts/update_add_admin.sh
+++ datacats/scripts/update_add_admin.sh
@@ -3,9 +3,9 @@
 
 . /usr/lib/ckan/bin/activate
 
-if /usr/lib/ckan/bin/ckanapi action user_show -c /project/development.ini id=admin; then
+if ckanapi action user_show -c /project/development.ini id=admin; then
     # Step 1: Grab the user's current info, step 2: add new pw, step 3: apply new info
     ckanapi action user_show id=admin -c /project/development.ini | python -c "from sys import stdin, stdout;from json import load, dump;obj = load(stdin);obj['password'] = load(open('/input/admin.json'))['password'];dump(obj, stdout)" | ckanapi action user_update -i -c /project/development.ini 
 else
-    /usr/lib/ckan/bin/ckanapi action user_create -i -c /project/development.ini < /input/admin.json
+    ckanapi action user_create -i -c /project/development.ini < /input/admin.json
 fi
