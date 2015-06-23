30597b1b09e5e7e22b47e5dd2a66c0fc3fdbc1a5
Jack McCracken <jackamc19@gmail.com>
2015-06-23 12:03:52 -0400

    Resetting the password of the admin user is now possible

--- test.py
+++
@@ -1,9 +0,0 @@
-from docker.client import Client
-from docker.utils import kwargs_from_env
-
-kwargs = kwargs_from_env()
-
-kwargs['tls'].verify = False
-
-client = Client(**kwargs)
-print client.version()
