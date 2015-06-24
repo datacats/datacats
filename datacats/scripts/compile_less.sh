945f1b6a1ea1772c2c45ec10e2e1225398130125
Jack McCracken <jackamc19@gmail.com>
2015-06-24 16:35:57 -0400

    'generate' CSS for prod also

--- datacats/scripts/compile_less.sh
+++ datacats/scripts/compile_less.sh
@@ -7,3 +7,5 @@ lessc /project/target/ckan/ckan/public/base/less/main.less \
 #TODO: We should actually minify
 lessc /project/target/ckan/ckan/public/base/vendor/bootstrap/less/bootstrap.less\
  > /project/target/ckan/ckan/public/css/bootstrap.min.css
+
+cp /project/target/ckan/ckan/public/base/css/main.debug.css /project/target/ckan/ckan/public/base/css/main.css
