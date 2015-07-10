c39a514fc54ebaec655a7a2b8063a3becc7ebbc9
Ian Ward <ian@excess.org>
2015-07-10 19:11:16 -0400

    more cli parsing tests

--- datacats/tests/test_cli_parsing.py
+++ datacats/tests/test_cli_parsing.py
@@ -7,6 +7,7 @@
 from unittest import TestCase
 
 from datacats.cli.main import _subcommand_arguments
+from datacats.error import DatacatsError
 
 
 def _s(cmd):
@@ -23,3 +24,18 @@ class TestParseArguments(TestCase):
 
     def test_help_short_option(self):
         self.assertEqual(_s('-h'), (None, '-h'))
+
+    def test_bad_subcommand(self):
+        self.assertRaises(DatacatsError, _s, 'whatsup')
+
+    def test_help_subcommand(self):
+        self.assertEqual(_s('help info'), ('info', '--help info'))
+
+    def test_subcommand(self):
+        self.assertEqual(_s('info'), ('info', 'info'))
+
+    def test_subcommand_option_first(self):
+        self.assertEqual(_s('-q info'), ('info', '-q info'))
+
+    def test_subcommand_option_last(self):
+        self.assertEqual(_s('info -q'), ('info', 'info -q'))
