6d59aaa876fea5688356865f2cadeea106068d2a
Jack McCracken <jackamc19@gmail.com>
2015-07-10 11:47:44 -0400

    Add datacats to sudoers in test image and entrypoint now adds to bash_profile

--- docker/test/entrypoint.sh
+++ docker/test/entrypoint.sh
@@ -4,7 +4,7 @@ while ! docker ps > /dev/null 2>&1; do
     sleep 0.01
 done
 export HOME=~datacats/
-source /home/datacats/venv/bin/activate
 cd /datacats
 python setup.py develop
+echo 'source /home/datacats/venv/bin/activate' > /home/datacats/.bash_profile
 sudo -u datacats "$@"
