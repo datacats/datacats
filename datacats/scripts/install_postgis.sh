4a5daff6c6b35cc991f4f05fb4822fea65e94ac2
Jack McCracken <jackamc19@gmail.com>
2016-02-14 22:55:19 -0800

    Fix postgis version bug

--- datacats/scripts/install_postgis.sh
+++ datacats/scripts/install_postgis.sh
@@ -16,7 +16,7 @@ done
 
 psql -c 'SELECT * FROM geometry_columns LIMIT 1;' 2> /dev/null && exit 0
 
-psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql
-psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql
+psql -f /usr/share/postgresql/9.3/contrib/postgis-2.2/postgis.sql
+psql -f /usr/share/postgresql/9.3/contrib/postgis-2.2/spatial_ref_sys.sql
 psql -c 'ALTER VIEW geometry_columns OWNER TO ckan;'
 psql -c 'ALTER TABLE spatial_ref_sys OWNER TO ckan;'
