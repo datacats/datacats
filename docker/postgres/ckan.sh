b1b167ab12aafec62f03fb7c3b3daa7e4015cc08
Ian Ward <ian@excess.org>
2017-01-02 16:10:15 -0500

    quick hack to set up datastore db correctly for ckan master

--- docker/postgres/ckan.sh
+++ docker/postgres/ckan.sh
@@ -19,6 +19,27 @@ GRANT USAGE ON SCHEMA public TO ckan_datastore_readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO ckan_datastore_readonly;
 
 ALTER DEFAULT PRIVILEGES FOR USER ckan_datastore_readwrite IN SCHEMA public GRANT SELECT ON TABLES TO ckan_datastore_readonly;
+
+CREATE OR REPLACE VIEW "_table_metadata" AS
+    SELECT DISTINCT
+        substr(md5(dependee.relname || COALESCE(dependent.relname, '')), 0, 17) AS "_id",
+        dependee.relname AS name,
+        dependee.oid AS oid,
+        dependent.relname AS alias_of
+        -- dependent.oid AS oid
+    FROM
+        pg_class AS dependee
+        LEFT OUTER JOIN pg_rewrite AS r ON r.ev_class = dependee.oid
+        LEFT OUTER JOIN pg_depend AS d ON d.objid = r.oid
+        LEFT OUTER JOIN pg_class AS dependent ON d.refobjid = dependent.oid
+    WHERE
+        (dependee.oid != dependent.oid OR dependent.oid IS NULL) AND
+        (dependee.relname IN (SELECT tablename FROM pg_catalog.pg_tables)
+            OR dependee.relname IN (SELECT viewname FROM pg_catalog.pg_views)) AND
+        dependee.relnamespace = (SELECT oid FROM pg_namespace WHERE nspname='public')
+    ORDER BY dependee.oid DESC;
+ALTER VIEW "_table_metadata" OWNER TO "ckan_datastore_readwrite";
+GRANT SELECT ON "_table_metadata" TO "ckan_datastore_readonly";
 EOSQL
 
 # Adjust PostgreSQL configuration so that remote connections to the
